d6827ec6185cea987284c490f46ffa7444233c34
김수환 <zzanggoon8@hiconsy.com>
2022-05-19 20:18:05 +0900

    2th, v-for Data Binding

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <div>
-    <!-- HTML 속성을 데이터 바인딩하고 싶으면 앞에 세미콜론(:) -->
-    <h4 :style="color">xx 원룸</h4>
-    <p>{{ dataBinding }} 만원</p>
+  <!-- v-for 상단 메뉴 커스텀-1 -->
+  <div class="menu">
+    <a v-for="menuCustom in Menus" :key="menuCustom">{{menuCustom}}</a>
+  </div>
+
+  <!-- v-for 데이터 커스텀-2 -->
+  <div v-for="(menuCustom, i) in Products" :key="i">
+    <!-- 첫 번째 방법 -->
+    <h4>{{Products[i]}} 원룸</h4>
+    <!-- 두 번째 방법-->
+    <h4>{{menuCustom}}</h4>
+    <p>60 만원</p>
   </div>
 </template>
 
 <script>
-/* TODO 생자 자바스크립트로 하는 데이터 바인딩
-  document.getElementById().innerHTML ??
-*/
 
 export default {
   name: 'App',
-  // TODO 데이터 보관함.
-  //  데이터는 object 자료로 저장해두기 {자료이름 : 자료내용}
   data() {
     return {
-      // 데이터 데이터 바인딩
-      dataBinding : 60,
-      // HTML 속성 데이터 바인딩
-      color: 'color: red',
+      // Data 데이터 바인딩
+      Products: ['삼성동', '인사동', "천호동"],
+      Menus: ['Home', "Products", "About"],
     }
   },
-  components: {}
 }
 </script>
 
-<style></style>
+<style>
+#app {
+  text-align: center;
+}
+.menu {
+  background-color: aquamarine;
+  padding: 15px;
+  border-radius: 5px; /*네모박스 모서리 깍기*/
+}
+.menu a {
+  color: red;
+  padding: 10px; /*Menu/Home/About 사이 간격 넓히기*/
+}
+</style>
