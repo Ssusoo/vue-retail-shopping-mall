fa55e2400e102b824fcf6f6e7b19dde1fe0e83e7
김수환 <zzanggoon8@hiconsy.com>
2022-09-19 21:55:07 +0900

    4th, 이미지(src) 경로

--- src/_4/ImageRoute.vue
+++ src/_4/ImageRoute.vue
@@ -1,5 +1,5 @@
 <!--
-	4th, 이미지
+	4th, 이미지(src)
 		절대경로 : http:// ~
 		상대경로 : 내가 같은 폴더의 이미지를 넣고 싶을 때
 	TODO 내가 -src에서 가져올 때 경로는 './'부터 시작한다!!!
@@ -15,19 +15,19 @@
 
 	<div>
 		<!-- TODO 1) 이미지 경로 지정 src -->
-		<img src="./images/room0.jpg" class="room-img">
+		<img :src="require(`@/assets/images/room0.jpg`)" class="room-img">
 		<h4>{{ products[0]}}</h4>
 		<p>50만원</p>
 		<button @click="eventHandlerCount[0]+=1">허위매물신고</button> <span>신고수 : {{ eventHandlerCount[0] }}</span>
 	</div>
 	<div>
-		<img src="./images/room1.jpg" class="room-img">
+		<img :src="require(`@/assets/images/room1.jpg`)" class="room-img">
 		<h4>{{ products[1]}}</h4>
 		<p>50만원</p>
 		<button @click="eventHandlerCount[1]+=1">허위매물신고</button> <span>신고수 : {{ eventHandlerCount[1] }}</span>
 	</div>
 	<div>
-		<img src="./images/room2.jpg" class="room-img">
+		<img :src="require(`@/assets/images/room2.jpg`)" class="room-img">
 		<h4>{{ products[2]}}</h4>
 		<p>50만원</p>
 		<button @click="eventHandlerCount[2]+=1">허위매물신고</button> <span>신고수 : {{ eventHandlerCount[2] }}</span>
