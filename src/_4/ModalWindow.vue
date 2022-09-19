5a5848895a0e7db41f95ff3955c30dcbdc58299c
김수환 <zzanggoon8@hiconsy.com>
2022-09-19 22:31:29 +0900

    4th, 모달창

--- src/_4/ModalWindow.vue
+++ src/_4/ModalWindow.vue
@@ -1,14 +1,32 @@
 <!--
 	4th, 모달창
+		1) vue에서 모달창 UI 만들기
+		2) vue 모달창 CSS 스타일 입히기
+
+		* TODO vue-router 설치하면 다른 페이지도 만드는 것도 가능함(다음에 하기로)
+
+		* TODO 동적인 UI 만드는 법
+		0. HTML CSS로 디자인하기(기본)
+		1. UI의 현재 상태를 데이터로 저장해둠.
+		2. 데이터에 따라 UI가 어떻게 보일지 작성
 -->
 <template>
 	<div class="menu">
 		<a v-for="(menu, i) in menus" :key="i">{{ menu }}</a>
 	</div>
+	<!-- TODO 1) 모달창 UI 만들기-->
+	<!-- TODO 4) 모달창 조건문으로(true일 때 열리기)-->
+	<div class="black-bg" v-if="openModalWindow == true">
+		<div class="white-bg">
+			<h4>상세페이지임</h4>
+			<p>상세페이지 내용임</p>
+		</div>
+	</div>
 
 	<div>
 		<img :src="require(`@/assets/images/room0.jpg`)" class="room-img">
-		<h4>{{ products[0] }}</h4>
+		<!-- TODO 5) 모달창 열기-->
+		<h4 @click="openModalWindow = true">{{ products[0] }}</h4>
 		<p>50만원</p>
 	</div>
 	<div>
@@ -21,7 +39,6 @@
 		<h4>{{ products[2] }}</h4>
 		<p>50만원</p>
 	</div>
-
 </template>
 
 <script>
@@ -29,6 +46,8 @@ export default {
 	name: 'ModalWindow',
 	data() {
 		return {
+			// TODO 3) 모달창의 상태 저장하기
+			openModalWindow: false,
 			menus: ['Home', 'Product', 'About'],
 			products: ['신림동원룸', '구로동원룸', '역삼동원룸']
 		}
@@ -37,6 +56,32 @@ export default {
 </script>
 
 <style>
+/* TODO 2) vue 모달창 CSS 스타일 입히기(body, div, black-bg, white-bg */
+body {
+	margin: 0px;
+}
+div {
+	box-sizing: border-box;
+}
+.black-bg {
+	width: 100%; height: 100%;
+	background: rgba(0,0,0,0.5);
+	position: fixed;
+	padding: 20px;
+}
+.white-bg {
+	width: 100%;
+	background: white;
+	border-radius: 8px;
+	padding: 20px;
+}
+
+/* 이미지 경로 지정 src */
+.room-img {
+	width: 100%;
+	margin-top: 40px;
+}
+
 .menu {
 	background: burlywood;
 	padding: 15px;
@@ -46,11 +91,5 @@ export default {
 .menu a {
 	color: black;
 	padding: 10px;
-}
-
-/* 이미지 경로 지정 src */
-.room-img {
-	width: 100%;
-	margin-top: 40px;
 }
 </style>
