b6620cdd05c09a143bb1ba0fb5d63d8cc80bb2ff
zzanggoon8 <zzanggoon8@gmail.com>
2022-09-23 00:29:10 +0900

    8th, 프록스(props) 문법

---
+++ src/_8/PropsGrammer.vue
@@ -0,0 +1,107 @@
+<!--
+	8th, 프록스(props) 문법
+		모달창 컴포넌트부터 만들자
+			1) DiscountBanner.vue import하기
+			2) Components: {} 안에 등록하기
+			3) 등록한 <DiscountBanner/> 갖다 쓰기
+
+		TODO Props 문법
+			부모와 자식 컴포넌트로 나눌 수 있는데,
+			자식 컴포넌트가 부모가 갖고 있는 데이터를 쓰려면
+			props로 데이터를 전송해야 한다.
+
+		TODO props로 자식 컴포넌트에게 데이터 보내는 방법
+			1) 데이터 전송하기(부모 컴포넌트에서)
+				v-bin: 혹은 세미콜론 :데이터="데이터"
+			2) 데이터 등록하기(자식 컴포넌트에서)
+			3) 데이터 갖다쓰기(자식 컴포넌트에서)
+
+		TODO 꿀팁 !!!
+			애초에 자식 컴포넌트에 데이터를 저장하면 되는 거 아닌가 할 수 있지만
+			부모도 쓰는 데이터라면 애초에 "부모 컴포넌트에 만들어주는 게 좋음".
+-->
+<template>
+	<div class="menu">
+		<a v-for="(menu, i) in menus" :key="i">{{ menu }}</a>
+	</div>
+
+	<!-- TODO 3) 등록한 <DiscountBanner/> 갖다 쓰기 -->
+	<ModalWindow
+			:rooms="rooms"
+			:userClickEvent="userClickEvent"
+			:openModalWindow="openModalWindow"
+	/>
+
+	<div v-for="(room, i) in rooms" :key="i">
+		<img :src="room.image" class="room-img">
+		<h4 @click="openModalWindow = true; userClickEvent = [i]">{{ room.title }}</h4>
+		<p>{{ room.price }}원</p>
+		<DiscountBanner/>
+	</div>
+</template>
+
+<script>
+import Data from '@/assets/jsons/oneRoom'
+import DiscountBanner from "@/components/DiscountBanner";
+// TODO 1) ModalWindow.vue import하기
+import ModalWindow from "@/components/ModalWindow";
+
+export default {
+	name: 'PropsGrammer',
+	components: {
+		// TODO 2) Components: {} 안에 등록하기
+		ModalWindow,
+		DiscountBanner,
+	},
+	data() {
+		return {
+			userClickEvent: 0,
+			eventHandlerCount: [0, 0, 0],
+			openModalWindow: false,
+			rooms: Data,
+			menus: ['Home', 'Product', 'About'],
+		}
+	},
+}
+</script>
+
+<style>
+.discount {
+	background-color: aqua;
+	padding: 10px;
+	margin: 10px;
+	border-radius: 5px;
+}
+body {
+	margin: 0px;
+}
+div {
+	box-sizing: border-box;
+	text-align: center;
+}
+.black-bg {
+	background-color: rgba(0,0,0,0.5);
+	padding: 20px;
+	width: 100%;
+	height: 100%;
+	position: fixed;
+}
+.white-bg {
+	background-color: white;
+	border-radius: 5px;
+	padding: 15px;
+}
+.room-img {
+	width: 100%;
+}
+.menu {
+	text-align: left;
+	background-color: burlywood;
+	padding: 15px;
+	border-radius: 5px;
+}
+.menu a {
+	color: black;
+	padding: 10px;
+}
+</style>
---
+++ src/components/ModalWindow.vue
@@ -0,0 +1,46 @@
+<!--
+	8th, Props 문법 (모달창 컴포넌트 만들기)
+		실제 ModalWindowComponent.vue에 있는 모달창을 컴포넌트로 바꾸는 작업에서
+		데이터 바인딩한 데이터가 실제 모달 컴포넌트에 없기 때문에 제목을 눌러도 얼리지 않는 오류가 생김
+
+		TODO 어떻게 해결할 수 있을까?
+		1) ModalWindowComponent.vue에서 데이터저장소에 저장한 데이터를 복붙해올까? 그건 별로다
+		2) 데이터는 한 곳에 보관하돼 필요하면 데이터를 가져다 쓰는 방식이 제일 좋다.
+
+		TODO 보관한 데이터를 어떻게 가져올까?
+			그게 바로 props 문법이다.
+
+		TODO props는 read-only임.
+			받아온 걸 수정하면 안된다.
+			그 말은 openModalWindow = false(x) 수정된 상태이기 때문에 안되는 거임.
+-->
+<template>
+	<div class="black-bg" v-if="openModalWindow === true">
+		<div class="white-bg">
+			<h4>상품번호 : {{ rooms[userClickEvent].id }}번</h4>
+			<p>{{ rooms[userClickEvent].title }}</p>
+			<p>{{ rooms[userClickEvent].content}}</p>
+			<!-- TODO 데이터를 수정한 거기 때문에 props가 에러가 남. -->
+<!--			<button @click="openModalWindow = false">닫기</button>-->
+			<DiscountBanner/>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'ModalWindow',
+	// TODO 2) 자식 컴포넌트에서 props 데이터 등록하기
+	props: {
+		// 데이터이름: 자료형 이름(Number, String, Array, Object ... etc)
+		rooms: Object,
+		userClickEvent: Number,
+		// TODO 위에 처럼 받아온 props는 수정했기 때문에 에러가 남.
+		openModalWindow: Boolean,
+	}
+}
+</script>
+
+<style>
+
+</style>
