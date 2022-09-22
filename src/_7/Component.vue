3e515f9dcb59aa750356ec2b69878d3ffe42e56d
zzanggoon8 <zzanggoon8@gmail.com>
2022-09-22 23:46:28 +0900

    7th, 컴포넌트 만들기(할인배너 컴포넌트)

--- src/_7/Component.vue
+++ src/_7/Component.vue
@@ -1,32 +1,70 @@
 <!--
 	7th, 컴포넌트 만들기
--->
+		: 긴 HTMl을 한 단어로 줄일 수 있는 문법
 
+		1) 컴포넌트를 위해 홍보 UI 만들기(할인 배너 만들기)
+			: 엄청 길다고 가정해보고 그걸 컴포넌트로 만들어 줄 거임.
+			* TODO 실제 HTML 코드를 한 글자로 축약할 수 있다면? <DiscountBanner/>
+				-> 그게 바로 컴포넌트 문법이다.
+		2) 할인 배너 CSS 스타일 주기
+		3) 컴포넌트 문법을 사용하기 위해서 component 디렉토리에서 HTML을 만들어 주어야 함.
+		4) 할인 배너 HTML 태그 Discount.vue로 옮기기
+		5) TODO DiscountBanner.vue(컴포넌트로)로 옮긴 할인 배너 Component.vue에서 사용하는 방법
+			5-1) DiscountBanner.vue import하기
+			5-2) Components: {} 안에 등록하기
+			5-3) 등록한 <DiscountBanner/> 갖다 쓰기
+
+	* TODO Question? 굳이 이런 컴포넌트 문법을 왜 쓸까?
+		1) 아름다워서
+		2) 재사용성이 쉬움
+		3) 컴포넌트로 축약을 했으면 20%에서 30%로 수정할 때
+			컴포넌트만 바꿔주면 100개를 바꾸는 게 아니라 하나만 끝나면 된다.
+
+-->
 <template>
+	<!-- TODO 1) 컴포넌트를 위해 홍보 UI 만들기(엄청 길다고 가정해보자) -->
+	<!-- TODO 3) 아래 할인 배너를 Dicount.vue로 옮기기-->
+<!--	<div class="discount">-->
+<!--		<h4>지금결제하면 20% 할인</h4>-->
+<!--	</div>-->
+	<!-- TODO 5-3) 등록한 <DiscountBanner/> 갖다 쓰기 -->
+	<DiscountBanner/>
+
 	<div class="black-bg" v-if="openModalWindow === true">
 		<div class="white-bg">
-			<h4>상품번호 : 번</h4>
-			<p>{{ rooms[userClickEvent]}}</p>
-			<p>내용</p>
+			<h4>상품번호 : {{ rooms[userClickEvent].id }}번</h4>
+			<p>{{ rooms[userClickEvent].title }}</p>
+			<p>{{ rooms[userClickEvent].content}}</p>
 			<button @click="openModalWindow = false">닫기</button>
+			<!-- TODO *재사용성이 용이한 컴포넌트 * -->
+			<DiscountBanner/>
 		</div>
 	</div>
 
 	<div class="menu">
 		<a v-for="(menu, i) in menus" :key="i">{{ menu }}</a>
 	</div>
+
 	<div v-for="(room, i) in rooms" :key="i">
 		<img :src="room.image" class="room-img">
-		<h4 @click="openModalWindow = true; userClickEvent[i]">{{ room.title }}</h4>
+		<h4 @click="openModalWindow = true; userClickEvent = [i]">{{ room.title }}</h4>
 		<p>{{ room.price }}원</p>
 	</div>
 </template>
 
 <script>
 import Data from '@/assets/jsons/oneRoom'
+// TODO 5-1) Discount.vue import하기
+import DiscountBanner from "@/components/DiscountBanner";
 
 export default {
 	name: 'Component',
+	components: {
+		// TODO 5-2) Components: {} 안에 등록하기
+		DiscountBanner,
+		// 작명 : import Name
+		// DiscountBanner: DiscountBanner,
+	},
 	data() {
 		return {
 			userClickEvent: 0,
@@ -40,6 +78,13 @@ export default {
 </script>
 
 <style>
+/* TODO 2) 할인 배너 CSS 스타일 주기 */
+.discount {
+	background-color: aqua;
+	padding: 10px;
+	margin: 10px;
+	border-radius: 5px;
+}
 body {
 	margin: 0px;
 }
---
+++ src/components/DiscountBanner.vue
@@ -0,0 +1,19 @@
+<template>
+	<!-- TODO 4) Component.vue에 있던 할인 배너 HTMl 태그 Discount.vue로 복붙 -->
+	<div class="discount">
+		<h4>지금결제하면 20% 할인</h4>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'DiscountBanner',
+}
+</script>
+
+<style>
+/*
+	TODO .discount CSS 스타일은 Component.vue에서 쓰나
+  Discount.vue에서 써주나 같다. 위치는 상관 없음.
+*/
+</style>
