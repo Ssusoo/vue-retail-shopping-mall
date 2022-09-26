0b5187e69c0e8c9567aa5cbb54dca7053a10f8e1
zzanggoon8 <zzanggoon8@gmail.com>
2022-09-26 23:06:39 +0900

    9th, Props 문법

---
+++ src/_9/PropsGrammerNumberString.vue
@@ -0,0 +1,25 @@
+<!--
+	9th, Props 문법
+		1) props 보낼 때 다양한 자료형 입력가능
+			1-1) 작명="문자자료"(String)
+			1-2) :작명="1234"(Number)
+-->
+<template>
+	<div>
+
+	</div>
+
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			object: {
+				name: 'kim',
+				age: 20,
+			}
+		}
+	},
+}
+</script>
---
+++ src/_9/PropsGrammerNumberStringComponent.vue
@@ -0,0 +1,19 @@
+<!--
+	9th, Props 문법
+		부모 컴포넌트에서 보낸 오브젝트 자료형 받기
+-->
+<template>
+	<div class="discount">
+		<h4>지금 결제하면 20%로 할인</h4>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'PropsGrammerNumberStringComponent',
+}
+</script>
+
+<style>
+
+</style>
