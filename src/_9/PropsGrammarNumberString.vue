e05cd4878491dfb60455fa9b30dac1d5a6b1517f
김수환 <zzanggoon8@hiconsy.com>
2022-09-27 11:33:16 +0900

    9th, Props 문법(Props로 데이터 보내기)

---
+++ src/_9/PropsGrammarNumberString.vue
@@ -0,0 +1,33 @@
+<!--
+	9th, Props 문법(Props로 데이터 보내기)
+		props 보낼 때 다양한 자료형 입력가능
+			작명="문자자료"(String)
+			:작명="1234"(Number)
+-->
+<template>
+	<!-- TODO 1) props로 데이터 보내기 -->
+	<!-- 둘 다 같은 의미임.
+		v-bind="object"(전체 보내기) == :name="objects.name", :age="objects.age"(개별적으로 보내고 싶을 때)
+	-->
+	<PropsGrammarNumberStringComponent
+			:objects="objects"
+	/>
+</template>
+
+<script>
+import PropsGrammarNumberStringComponent from '@/_9/PropsGrammarNumberStringComponent'
+
+export default {
+	components: {
+		PropsGrammarNumberStringComponent,
+	},
+	data() {
+		return {
+			objects: {
+				name: 'kim',
+				age: 20,
+			}
+		}
+	},
+}
+</script>
