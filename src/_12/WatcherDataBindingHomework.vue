851b3e96d32c3ed6d77173289de7f5ae41d1afb5
김수환 <zzanggoon8@hiconsy.com>
2022-09-28 10:50:51 +0900

    12th, Watcher Grammar 숙제(입력한 데이터 초기화하기)

--- src/_12/WatcherDataBindingHomework.vue
+++ src/_12/WatcherDataBindingHomework.vue
@@ -1,5 +1,13 @@
 <!--
 	12th, Watcher Grammar 숙제(입력한 데이터 초기화하기)
+		TODO 해결방법 isNaN() 함수 사용하기 !!!
+			자바스크립트로 숫자인지 문자인지 판단하기 위해서
+			isNaN() 함수 안에 숫자를 입력하면 false, 글자를 입력하면 true가 나온다.
+			'123'이라는 숫자인척하는 글자도 false가 나옴
+
+
+		TODO 1) 숫자만 입력하라는 alert() 창 띄우기
+		TODO 2) month의 초기값을 1로 다시 초기화 하기
 -->
 <template>
 	<div class="menu">
@@ -28,8 +36,12 @@ export default {
 	},
 	watch: {
 		month(userData) {
-			if (userData > 13) {
-				alert('13이상 입력하지 마세요.')
+			// TODO false면 숫자, true면 문자
+			if (isNaN(userData) == true) {
+				// TODO 1) 숫자만 입력하라는 alert() 창 띄우기
+				alert('문자를 입력하지 마세요.')
+				// TODO 2) month의 초기값을 1로 다시 초기화 하기
+				this.month = 1;
 			}
 		},
 	},
