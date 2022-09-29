555643eef24b8e67b30174225f1c4f2d46fb0b29
김수환 <zzanggoon8@hiconsy.com>
2022-09-29 12:34:44 +0900

    14th, 뷰 라이프 사이클

--- src/_14/VueLifeCycle.vue
+++ src/_14/VueLifeCycle.vue
@@ -5,7 +5,7 @@
 
 		1) Create 단계 : 데이터만 존재하는 단계
 		2) Mount 단계 : <template> 사이에 있던 걸 실제 HTML로 바꿔 줌
-		3) 컴포넌트 생성 : index.html에 장착 된다.
+		3) 컴포넌트 생성 단계 : index.html에 장착 된다.
 		4) Update 단계 : data가 변하면 HTML 이 재렌더링된다고 했었는데,
 				실제로 컴포넌트가 재렌더링된다. 그걸 'Update'라고 한다.
 		5) Unmount 단계 : 강제를 삭제하면 컴포넌트가 삭제된다.
@@ -17,4 +17,70 @@
 			또 다른 예는 Update 단계(재렌더링) 전에 'Lifecycle Hook'을 통해 이것 좀 실행해주세요.
 			TODO * 혹은 Create 단계(데이터만 존재하는 단계)에서 Lifecycle Hook을 사용할 수도 있음.
 			TODO 결론, Lifecycle Hook을 사용하기 위해 'Lifecycle' 개념을 배우는 거다.
-계->
+
+		TODO Lifecycle Hook 사용하는 방법
+			1) <script> 사이사이에 딴지를 걸어 사용할 수 있다. mounted() {} -> before~
+			2) Discount 컴포넌트가 2초후에 사라지게 만들기
+				2-1) UI 현재 데이터(상태) 만들기
+				2-2) 그거에 따라 보이는지 안보이는지(setTimeout 함수사용하기)
+
+		TODO * 서버에서 데이터를 가져올 때도 Lifecycle Hook 안에 코드를 짠다.
+-->
+
+<template>
+	<div class="menu">
+		<a v-for="menu in menus" :key="menu">{{ menu }}</a>
+	</div>
+	<VueLifeCycleDiscountComponent
+		v-if="showDiscount === true"
+	/>
+</template>
+
+<script>
+import VueLifeCycleDiscountComponent from '@/_14/component/VueLifeCycleDiscountComponent'
+export default {
+	name: 'VueLifeCycle',
+	components: {
+		VueLifeCycleDiscountComponent,
+	},
+	// TODO 1) <script> 사이사이에 딴지를 걸어 사용할 수 있다. mounted() {} -> before~
+	//  mount 후에 뭔가 실행하고 싶을 때
+	mounted() {
+		// TODO 2-2) 그거에 따라 보이는지 안보이는지(setTimeout 함수사용하기)
+		//  그냥 함수가 아니라 ()=>{} (자바스크립트에서 제공하는 에러 function임.
+		setTimeout(()=> {
+			this.showDiscount = false;
+		}, 2000);
+	},
+	// TODO mount 되기 전에 뭔가 실행하고 싶을 때
+	beforeMount() {
+	},
+	data() {
+		return {
+			// TODO 2-1) UI 현재 데이터(상태) 만들기
+			showDiscount: true,
+			menus: ['Home', 'Product', 'About'],
+		}
+	},
+}
+</script>
+
+<style>
+body {
+	margin: 0px;
+}
+.menu{
+	background-color: aquamarine;
+	padding: 20px;
+	text-align: left;
+}
+.menu a {
+	padding: 15px;
+}
+.discount {
+	text-align: center;
+	background-color: burlywood;
+	padding: 20px;
+	border-radius: 5px;
+}
+</style>
--- src/_14/component/VueLifeCycleDiscountComponent.vue
+++ src/_14/component/VueLifeCycleDiscountComponent.vue
@@ -1,9 +1,21 @@
 <template>
-
+	<div>
+		<h4 class="discount">지금 구매하면 30% 할인</h4>
+	</div>
 </template>
 
 <script>
 export default {
+	name: 'VueLifeCycleDiscountComponent',
+	components: {
 
+	},
+	data() {
+
+	}
 }
 </script>
+
+<style>
+
+</style>
